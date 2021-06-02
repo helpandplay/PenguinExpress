a2f40ffe53432acaf5ecb93777fcbf6406cf9d74
helpandplay <helpandplay.kr@gmail.com>
2021-06-02 10:26:54 +0900

    add mysql connection

---
+++ PenguinExpress/Main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace PenguinExpress
+{
+  public partial class Main : Form
+  {
+    MySqlCommand cmd;
+    MySqlConnection connection;
+    public Main()
+    {
+      InitializeComponent();
+      onConnect();
+    }
+    private void onConnect()
+    {
+      string server = Env.SERVER;
+      string port = Env.PORT;
+      string uid = Env.ADMIN_UID;
+      string pwd = Env.ADMIN_PWD;
+      string db = Env.DBNAME;
+
+      string conn = string.Format("Server={0};Port={1};Uid={2};Pwd={3};Database={4};CHARSET=UTF8", server, port, uid, pwd, db);
+      connection = new MySqlConnection(conn);
+      try
+      {
+        connection.Open();
+        cmd = new MySqlCommand("", connection);
+      }catch(Exception error)
+      {
+        Debug.WriteLine(error.Message);
+      }
+      finally
+      {
+        Debug.Close();
+      }
+    }
+  }
+}
