d953286b3bc586f3e20ad3a822ecbd81097b5185
helpandplay <helpandplay.kr@gmail.com>
2021-06-08 14:45:19 +0900

    fix graph bug

--- PenguinExpress/Status.cs
+++ PenguinExpress/Status.cs
@@ -25,19 +25,19 @@ namespace PenguinExpress
     {
       switch (key)
       {
-        case 1:
+        case 0:
           return "의류";
-        case 2:
+        case 1:
           return "전자제품류";
-        case 3:
+        case 2:
           return "식품류";
-        case 4:
+        case 3:
           return "식기류";
-        case 5:
+        case 4:
           return "가구류";
-        case 6:
+        case 5:
           return "의약품";
-        case 7:
+        case 6:
           return "기타";
         default:
           throw new Exception("알 수 없는 품목번호입니다. 품목번호 : " + key);
