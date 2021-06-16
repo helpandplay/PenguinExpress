176d134414f4acaec4b964f04a25f873157861db
helpandplay <helpandplay.kr@gmail.com>
2021-06-16 14:18:50 +0900

    add complete serivce, entity

---
+++ PenguinExpress/entity/Complete.entity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenguinExpress.entity
+{
+  public class CompleteEntity
+  {
+    private static CompleteEntity instance = null;
+
+    public readonly string id = "id";
+    public readonly string trackingID = "tracking_id";
+    public readonly string employeeID = "e_id";
+    public readonly string sellerAddr = "s_addr";
+    public readonly string sellerPhone = "s_phone";
+    public readonly string prodName = "p_name";
+    public readonly string prodQty = "p_qty";
+    public readonly string prodCode = "p_code";
+    public readonly string buyAddr = "b_addr";
+    public readonly string buyPhone = "b_phone";
+    public readonly string buyRegionCode = "b_region_code";
+    public readonly string rvTime = "rv_time";
+    public readonly string cpTime = "cp_time";
+
+    private CompleteEntity()
+    {
+
+    }
+    public static CompleteEntity getInstance()
+    {
+      if( instance == null)
+      {
+        instance = new CompleteEntity();
+      }
+      return instance;
+    }
+  }
+}
---
+++ PenguinExpress/service/Complete.service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PenguinExpress.entity;
+
+namespace PenguinExpress.service
+{
+  class CompleteService
+  {
+    private readonly CompleteEntity entity = CompleteEntity.getInstance();
+    public CompleteService(){}
+    public Dictionary<string, string> findAll()
+    {
+
+    }
+    public Dictionary<string, string> findOne(string trackingID)
+    {
+
+    }
+    public bool addDelivery(Dictionary<string, string> data)
+    {
+
+    }
+  }
+}
