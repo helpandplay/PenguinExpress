f718c706453fcf7bb322eddcbef028c1d89b1b06
helpandplay <helpandplay.kr@gmail.com>
2021-06-16 10:25:09 +0900

    add style

---
+++ PenguinExpress/config/Styles.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace PenguinExpress.config
+{
+  public static class Styles
+  {
+    //color
+    public const string baseColor = "#fbeed2";
+    public const string baseStrongColor = "#dcc8b4";
+    public const string contentStrongColor = "#b89068";
+    public const string textColor = "#493929";
+    public const string textBrightColor = "#625841";
+
+    public const string primary = " #1266F1";
+    public const string light = "#FBFBFB";
+    public const string dark = "#262626";
+    public const string success = "#00B74A";
+    public const string danger = "#F93154";
+    public const string warning = "#FFA900";
+    public const string info = "#39C0ED";
+    public const string blueGray = "#ECEFF1";
+    //font
+
+    public static Font boldFont = new Font("맑은 고딕", 11, FontStyle.Bold);
+    public static Font font = new Font("맑은 고딕", 11, FontStyle.Regular);
+
+  }
+}
